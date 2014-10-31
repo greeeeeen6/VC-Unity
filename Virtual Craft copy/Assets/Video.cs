65c045424b12b198bc65561bc1f457696b7ac775
greeeeeen6 <graduatestudy@Graduates-MacBook-Pro.local>
2014-10-31 10:57:31 +0900

    video update

---
+++ Virtual Craft copy/Assets/Video.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class Video : MonoBehaviour {
+
+	public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = movie as MovieTexture;
+		movie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
